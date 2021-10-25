a132431ff03c80b05cb423927b19c848d7698d40
Amar Ma'ruf <51941294+amar-maruf@users.noreply.github.com>
2021-10-25 20:02:36 +0700

    Buat Latihan
    Nyoba

---
+++ latihan/konstanta.php
@@ -0,0 +1,9 @@
+<?php
+define ("Hobi", "Main Komputer");
+echo "Halo temen-temen, hobi saya : ";
+echo Hobi;
+echo "<br>";
+$jl=67237;
+echo "dan saya tinggal di Sumber Taman, Probolinggo, ";
+echo $jl;
+?>
